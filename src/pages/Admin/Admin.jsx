01b461f074d834d058e46d98792c196f0a580a4e
TomoriComAncora <lucas.fernando@ufms.br>
2024-04-17 19:18:22 -0400

    começando o admin

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,11 +1,27 @@
-import './Admin.css'
+import { useState } from "react";
+import "./Admin.css";
 
 function Admin() {
+  const [tarefaInput, setTarefaInput] = useState("");
+  const [tarefas, setTarefas] = useState([]);
+
+  const handleForm = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-        <h1>Admin</h1>
+    <div className="admin-container">
+      <h1>Minhas tarefas</h1>
+      <form className="login" onSubmit={handleForm}>
+        <textarea
+          placeholder="Digite sua tarefa..."
+          value={tarefaInput}
+          onChange={(e) => setTarefaInput(e.target.value)}
+        ></textarea>
+        <button type="submit">Registrar tarefa</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Admin
+export default Admin;
